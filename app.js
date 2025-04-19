7d703f406750b382e9d9789e0489fa7725d288fc
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-04-19 16:20:14 +0500

    some changings

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-var masjadImg =
+var masjidImg =
   "https://en.numista.com/catalogue/photos/pakistan/432-original.jpg";
 
 var chandImg =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3y2mJC4vXj_g4BJ-NqoldCqcGe5SOOHuXIA&s";
 
-var para = document.querySelector("#para");
+var para_1 = document.querySelector("#para_1");
+var para_2 = document.querySelector("#para_2");
 var img = document.querySelector("#img");
-img.src = masjadImg;
+img.src = chandImg;
+
+
 
 function toss(coin) {
   var randomNum = Math.floor(Math.random() * 2);
   console.log(randomNum);
 
-  if (
-    (coin === "masjad" && randomNum === 0) ||
-    (coin === "chand" && randomNum === 1)
-  ) {
-    para.innerHTML = "YOU WON";
+
+  if (coin === "masjid" && randomNum === 0) {
+    para_1.innerHTML = "YOU WON";
+    para_2.innerHTML = "YOU LOSS";
+    
+  }else if (coin === "chand" && randomNum === 1) {
+    para_1.innerHTML = "YOU LOSS";
+    para_2.innerHTML = "YOU WON";
+    
+  }else if (randomNum != 0 ) {
+    para_1.innerHTML = "YOU LOSS";
+    para_2.innerHTML = "YOU WON";
+    
+  }else if(randomNum != 1){
+    para_1.innerHTML = "YOU WON";
+    para_2.innerHTML = "YOU LOSS";
+
   } else {
-    para.innerHTML = "YOU LOSS";
+    para_1.innerHTML = "";
+    para_2.innerHTML = "";
   }
 
+
   if (randomNum === 0) {
-    img.src = masjadImg;
+    img.src = masjidImg;
+
   } else {
     img.src = chandImg;
+
   }
+
 }
